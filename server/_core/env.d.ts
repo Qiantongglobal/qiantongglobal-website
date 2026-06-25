19e05b9e33618ac759e736eb60a5377bd429c91f
Qiantongglobal <info@qiantongglobal.com>
2026-06-25 14:27:15 +0800

    Create env.d.ts

---
+++ server/_core/env.d.ts
@@ -0,0 +1,8 @@
+export declare const ENV: {
+    aiappPlatformOrigin: string;
+    supabaseUrl: string;
+    supabaseAnonKey: string;
+    appId: string;
+    corpId: string;
+    bucketName: string;
+};
