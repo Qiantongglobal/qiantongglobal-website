934318f5b875726f479c160fca7bdef117cd256b
Qiantongglobal <info@qiantongglobal.com>
2026-06-25 14:30:11 +0800

    Create index.d.ts

---
+++ server/services/contacts/index.d.ts
@@ -0,0 +1,3 @@
+import { IContactsProvider } from './interface.js';
+export declare function createContactsProvider(req: any): IContactsProvider;
+export type { IContactsProvider, Employee, Department, DepartmentNode, DepartmentTreeResult, DeptSearchItem, DeptSearchResult, SearchResult } from './interface.js';
